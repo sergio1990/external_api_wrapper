93a2ec8dd03ddeaced82ece0ddd030b50752720c
Sergio Gernyak <sergeg1990@gmail.com>
2018-03-27 14:22:50 +0300

    :rotating_light: tests setup

--- Gemfile
+++ Gemfile
@@ -6,3 +6,7 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 gemspec
 
 gem 'rubocop', '~> 0.53.0', require: false
+
+gem 'minitest', '5.11.3'
+gem 'minitest-reporters', '1.1.19', require: false
+gem 'mocha', '1.4.0', require: false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "external_api_wrapper"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'external_api_wrapper'
 
-require "minitest/autorun"
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use!
